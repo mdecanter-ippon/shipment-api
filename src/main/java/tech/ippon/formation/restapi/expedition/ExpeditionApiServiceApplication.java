6a104811fdaddff94ddefaf09539310c8f126649
Halim IZZOUZI <hizzouzi@ippon.fr>
2025-11-14 17:09:57 +0100

    Initial commit

---
+++ src/main/java/tech/ippon/formation/restapi/expedition/ExpeditionApiServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.ippon.formation.restapi.expedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpeditionApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpeditionApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/ippon/formation/restapi/expedition/ExpeditionApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ippon.formation.restapi.expedition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpeditionApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
